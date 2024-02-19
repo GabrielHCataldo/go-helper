980bae2e512922a7e184e09731a965c76ae9c0cb
Gabriel Cataldo <gabriel.cataldo@petzpartner.com.br>
2024-02-19 12:36:53 -0300

    Bug fix validate bytes

--- helper/type.go
+++ helper/type.go
@@ -419,9 +419,6 @@ func IsNotTime(a any) bool {
 
 // IsBytes If value is slice byte return true, otherwise return false
 func IsBytes(a any) bool {
-	if IsNil(a) {
-		return false
-	}
 	vr := reflect.ValueOf(a)
 	if IsPointer(a) || IsInterface(a) {
 		vr = vr.Elem()
