c9e00bb688fedb0907c2130afb939e2e448c0c15
Gabriel Cataldo <gabriel.cataldo@petzpartner.com.br>
2024-03-15 19:22:54 -0300

    Added min and max for float64 and float32.

--- helper/float.go
+++ helper/float.go
@@ -18,3 +18,35 @@ func RoundDown(v float64, precision int) float64 {
 func Round(v float64, precision int) float64 {
 	return math.Round(v*(math.Pow10(precision))) / math.Pow10(precision)
 }
+
+// MinFloat32 min float, if v is greater than min, min will prevail
+func MinFloat32(v, min float32) float32 {
+	if v < min {
+		return min
+	}
+	return v
+}
+
+// MaxFloat32 max float, if v is less than max, max will prevail
+func MaxFloat32(v, max float32) float32 {
+	if v > max {
+		return max
+	}
+	return v
+}
+
+// MinFloat64 min float, if v is greater than min, min will prevail
+func MinFloat64(v, min float64) float64 {
+	if v < min {
+		return min
+	}
+	return v
+}
+
+// MaxFloat64 max float, if v is less than max, max will prevail
+func MaxFloat64(v, max float64) float64 {
+	if v > max {
+		return max
+	}
+	return v
+}
