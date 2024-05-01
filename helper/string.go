f5e404b309ae9c2132843f0d7be3c08514c35b9e
Gabriel Cataldo <gabriel.cataldo@petzpartner.com.br>
2024-05-01 16:22:53 -0300

    Added parse any string to snake, camel, kebab case

--- helper/string.go
+++ helper/string.go
@@ -71,20 +71,20 @@ func GetFirstLastName(a any) string {
 
 // GetFileString get file by uri string param
 func GetFileString(uri string) (string, error) {
-	bytes, err := os.ReadFile(uri)
+	bs, err := os.ReadFile(uri)
 	if err != nil {
 		return "", err
 	}
-	return string(bytes), nil
+	return string(bs), nil
 }
 
 // GetFileJson get file by uri string param, fill dest by file json
 func GetFileJson(uri string, dest any) error {
-	bytes, err := os.ReadFile(uri)
+	bs, err := os.ReadFile(uri)
 	if err != nil {
 		return err
 	}
-	return json.Unmarshal(bytes, dest)
+	return json.Unmarshal(bs, dest)
 }
 
 // RandomNumberStr generate random numbers string by min and max parameters
@@ -114,6 +114,53 @@ func CompactString(a any) string {
 		}
 	}
 	return CleanAllRepeatSpaces(string(bs))
+}
+
+func CamelCaseString(a any) string {
+	s := SimpleConvertToString(a)
+
+	wordBoundary := regexp.MustCompile("[^a-zA-Z0-9]+")
+	words := wordBoundary.Split(s, -1)
+
+	for i := 0; i < len(words); i++ {
+		if words[i] == "" {
+			continue
+		}
+		if i == 0 {
+			words[i] = strings.ToLower(words[i])
+		} else {
+			words[i] = strings.Title(words[i])
+		}
+	}
+
+	return strings.Join(words, "")
+}
+
+func SnakeCaseString(a any) string {
+	s := SimpleConvertToString(a)
+
+	matchFirstCap := regexp.MustCompile("(.)([A-Z][a-z]+)")
+	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
+	snake := matchFirstCap.ReplaceAllString(s, "${1}_$2")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+
+	//remove all non-alphanumeric character except underscores
+	snake = regexp.MustCompile("[^a-zA-Z0-9_]+").ReplaceAllString(snake, "_")
+
+	return strings.ToLower(snake)
+}
+
+func KebabCaseString(a any) string {
+	s := SimpleConvertToString(a)
+
+	matchFirstCap := regexp.MustCompile("(.)([A-Z][a-z]+)")
+	matchAllCap := regexp.MustCompile("([a-z0-9])([A-Z])")
+
+	kebab := matchFirstCap.ReplaceAllString(s, "${1}-$2")
+	kebab = matchAllCap.ReplaceAllString(kebab, "${1}-${2}")
+	kebab = regexp.MustCompile("[^a-zA-Z0-9-]+").ReplaceAllString(kebab, "-")
+
+	return strings.ToLower(kebab)
 }
 
 // Sprintln get all values convert to text string
