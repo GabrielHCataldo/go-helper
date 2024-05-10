6484c914f283bddfebe1b36970f62fd92c050b13
Gabriel Cataldo <gabriel.cataldo@petzpartner.com.br>
2024-05-10 14:54:51 -0300

    Bugfix custom validator

--- helper/validator.go
+++ helper/validator.go
@@ -8,32 +8,34 @@ var customValidate *validator.Validate
 
 // Validate persist custom validator.Validate
 func Validate() *validator.Validate {
-	if customValidate == nil {
-		customValidate = validator.New()
-		_ = customValidate.RegisterValidation("http_method", validateHttpMethod)
-		_ = customValidate.RegisterValidation("url_path", validateUrlPath)
-		_ = customValidate.RegisterValidation("enum", validateEnum)
-		_ = customValidate.RegisterValidation("phone_us", validatePhoneUs)
-		_ = customValidate.RegisterValidation("phone_br", validatePhoneBr)
-		_ = customValidate.RegisterValidation("full_name", validateFullName)
-		_ = customValidate.RegisterValidation("bcrypt", validateBcrypt)
-		_ = customValidate.RegisterValidation("postal_code", validatePostalCode)
-		_ = customValidate.RegisterValidation("bearer", validateBearer)
-		_ = customValidate.RegisterValidation("before_now", validateBeforeNow)
-		_ = customValidate.RegisterValidation("before_today", validateBeforeToday)
-		_ = customValidate.RegisterValidation("after_now", validateAfterNow)
-		_ = customValidate.RegisterValidation("after_today", validateAfterToday)
-		_ = customValidate.RegisterValidation("today", validateToday)
-		_ = customValidate.RegisterValidation("now", validateNow)
-		_ = customValidate.RegisterValidation("full_now", validateFullNow)
-		_ = customValidate.RegisterValidation("cpf", validateCpf)
-		_ = customValidate.RegisterValidation("cnpj", validateCnpj)
-		_ = customValidate.RegisterValidation("cpfcnpj", validateCpfCnpj)
-		_ = customValidate.RegisterValidation("mongodb", validateMongoDb)
-		_ = customValidate.RegisterValidation("duration", validateDuration)
-		_ = customValidate.RegisterValidation("byte_unit", validateByteUnit)
-		_ = customValidate.RegisterValidation("megabyte_unit", validateMegabyteUnit)
+	if customValidate != nil {
+		return customValidate
 	}
+
+	customValidate = validator.New()
+	_ = customValidate.RegisterValidation("http_method", validateHttpMethod)
+	_ = customValidate.RegisterValidation("url_path", validateUrlPath)
+	_ = customValidate.RegisterValidation("enum", validateEnum)
+	_ = customValidate.RegisterValidation("phone_us", validatePhoneUs)
+	_ = customValidate.RegisterValidation("phone_br", validatePhoneBr)
+	_ = customValidate.RegisterValidation("full_name", validateFullName)
+	_ = customValidate.RegisterValidation("bcrypt", validateBcrypt)
+	_ = customValidate.RegisterValidation("postal_code", validatePostalCode)
+	_ = customValidate.RegisterValidation("bearer", validateBearer)
+	_ = customValidate.RegisterValidation("before_now", validateBeforeNow)
+	_ = customValidate.RegisterValidation("before_today", validateBeforeToday)
+	_ = customValidate.RegisterValidation("after_now", validateAfterNow)
+	_ = customValidate.RegisterValidation("after_today", validateAfterToday)
+	_ = customValidate.RegisterValidation("today", validateToday)
+	_ = customValidate.RegisterValidation("now", validateNow)
+	_ = customValidate.RegisterValidation("full_now", validateFullNow)
+	_ = customValidate.RegisterValidation("cpf", validateCpf)
+	_ = customValidate.RegisterValidation("cnpj", validateCnpj)
+	_ = customValidate.RegisterValidation("cpfcnpj", validateCpfCnpj)
+	_ = customValidate.RegisterValidation("mongodb", validateMongoDb)
+	_ = customValidate.RegisterValidation("duration", validateDuration)
+	_ = customValidate.RegisterValidation("byte_unit", validateByteUnit)
+	_ = customValidate.RegisterValidation("megabyte_unit", validateMegabyteUnit)
 	return customValidate
 }
 
@@ -103,15 +105,6 @@ func validatePostalCode(fl validator.FieldLevel) bool {
 }
 
 func validateMongoDb(fl validator.FieldLevel) bool {
-	if IsSliceType(fl.Field().Interface()) {
-		for i := 0; i < fl.Field().Len(); i++ {
-			fieldValueSlice := fl.Field().Index(i).Interface()
-			if IsNotObjectIdType(fieldValueSlice) {
-				return false
-			}
-		}
-		return true
-	}
 	return IsObjectIdType(fl.Field().Interface())
 }
 
@@ -128,15 +121,6 @@ func validateMegabyteUnit(fl validator.FieldLevel) bool {
 }
 
 func validateEnum(fl validator.FieldLevel) bool {
-	if IsSliceType(fl.Field().Interface()) {
-		for i := 0; i < fl.Field().Len(); i++ {
-			fieldValueSlice, ok := fl.Field().Index(i).Interface().(BaseEnum)
-			if !ok || !fieldValueSlice.IsEnumValid() {
-				return false
-			}
-		}
-		return true
-	}
 	value, ok := fl.Field().Interface().(BaseEnum)
 	return ok && value.IsEnumValid()
 }
